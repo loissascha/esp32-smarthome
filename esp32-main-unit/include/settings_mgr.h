b192a13073f8d092e738d14597e94d0192bc508f
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-01-26 11:25:50 +0100

    added daytime settings to settings mgr

--- esp32-main-unit/include/settings_mgr.h
+++ esp32-main-unit/include/settings_mgr.h
@@ -7,5 +7,9 @@ public:
 	SettingsMgr();
 	bool tischlampeManualMode;
 	bool tischlampeManualStatus;
+	int daytimeStartHour;
+	int daytimeStartMinute;
+	int daytimeEndHour;
+	int daytimeEndMinute;
 private:
 };
--- esp32-main-unit/src/settings_mgr.cpp
+++ esp32-main-unit/src/settings_mgr.cpp
@@ -3,4 +3,8 @@
 SettingsMgr::SettingsMgr() { 
 	tischlampeManualMode = false;
 	tischlampeManualStatus = false;
+	daytimeStartHour = 0;
+	daytimeStartMinute = 0;
+	daytimeEndHour = 0;
+	daytimeEndMinute = 0;
 };
