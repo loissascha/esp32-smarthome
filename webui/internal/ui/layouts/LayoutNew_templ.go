80d591d680cc22e2b4a82d951dfcdb57d13cd649
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-01-28 20:19:02 +0100

    temps stuff

--- webui/internal/ui/layouts/LayoutNew_templ.go
+++ webui/internal/ui/layouts/LayoutNew_templ.go
@@ -171,7 +171,7 @@ func LayoutNew() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 9, "</nav><div class=\"grow\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 9, "</nav><div class=\"grow grid\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
